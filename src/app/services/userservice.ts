31912dd163f121a5dd5bc5080112b756c8e8725e
LLA <lyes.lakehal@gmail.com>
2024-01-02 00:19:00 +0100

    correct end-points

--- src/app/services/userservice.ts
+++ src/app/services/userservice.ts
@@ -7,27 +7,27 @@ import { User } from '../domain/user';
 @Injectable()
 export class UserService {
 
-    api_url: string = 'http://localhost:8080/api/v1/';
+    api_url: string = 'http://localhost:8080';
 
     constructor(private httpClient: HttpClient) { }
 
     getUsers(): Observable<User[]> {
         return this.httpClient
-        .get<User[]>(this.api_url + 'user/');
+        .get<User[]>(this.api_url + '/api/users');
     }
 
     findUserByEmail(email: string): Observable<User> {
         return this.httpClient
-        .get<User>(this.api_url + 'user/email/' + email);
+        .get<User>(this.api_url + '/api/users/email/' + email);
     }
 
     saveUser(user: User): Observable<User> {
         return this.httpClient
-        .post<User>(this.api_url + 'user/save', user);
+        .post<User>(this.api_url + '/api/users', user);
     }
 
     deleteUser(id: string): Observable<any> {
         return this.httpClient
-        .delete<any>(this.api_url + 'user/delete/' + id);
+        .delete<any>(this.api_url + '/api/users/' + id);
     }
 }
