9b597833d06ad79824772ba2af4a9d796c2a07c0
LLA <lyes.lakehal@gmail.com>
2024-01-02 12:01:13 +0100

    Get apiUrl from environment

--- src/app/services/userservice.ts
+++ src/app/services/userservice.ts
@@ -1,33 +1,33 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import {Observable} from 'rxjs';
+import { environment } from '../../environments/environment';
 
 import { User } from '../domain/user';
 
 @Injectable()
 export class UserService {
 
-    api_url: string = 'http://localhost:8080';
-
     constructor(private httpClient: HttpClient) { }
 
     getUsers(): Observable<User[]> {
+        console.log(environment.apiUrl);
         return this.httpClient
-        .get<User[]>(this.api_url + '/api/users');
+        .get<User[]>(environment.apiUrl + '/api/users');
     }
 
     findUserByEmail(email: string): Observable<User> {
         return this.httpClient
-        .get<User>(this.api_url + '/api/users/email/' + email);
+        .get<User>(environment.apiUrl + '/api/users/email/' + email);
     }
 
     saveUser(user: User): Observable<User> {
         return this.httpClient
-        .post<User>(this.api_url + '/api/users', user);
+        .post<User>(environment.apiUrl + '/api/users', user);
     }
 
     deleteUser(id: string): Observable<any> {
         return this.httpClient
-        .delete<any>(this.api_url + '/api/users/' + id);
+        .delete<any>(environment.apiUrl + '/api/users/' + id);
     }
 }
